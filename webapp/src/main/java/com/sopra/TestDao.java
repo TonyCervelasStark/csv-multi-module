a2e8cd04612f0bda224c3c96ae420a0da6c231e5
Tony <antoine.banck@gmail.com>
2016-11-24 11:55:50 +0100

    web-app initialized

---
+++ webapp/src/main/java/com/sopra/TestDao.java
@@ -0,0 +1,17 @@
+package com.sopra;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.sopra.modele.TrainingDemand;
+
+@Stateless
+public class TestDao {
+	@PersistenceContext(unitName = "formations")
+	EntityManager em;
+
+	public TrainingDemand test() {
+		return em.find(TrainingDemand.class, 1256);
+	}
+}
---
+++ webapp/src/main/java/com/sopra/TestWS.java
@@ -0,0 +1,16 @@
+package com.sopra;
+
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/test")
+public class TestWS {
+	@EJB
+	TestDao dao;
+
+	@GET
+	public String test() {
+		return "hello " + dao.test();
+	}
+}
